f97041bf7087d328cefbcb127298149f7fa3f69b
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-01-05 19:47:42 +0100

    v2.0

--- src/models/Estado.java
+++ src/models/Estado.java
@@ -14,47 +14,7 @@ public class Estado {
 	
 	
 	//Getters y setters
-	public String getEstado(Estado Estado) {
-		String estado = "sin efecto";
-		
-		switch (Estado.estado.toLowerCase()) {
-		case "confuso":
-			estado = "confuso";
-			break;
-		case "maldito":
-			estado = "maldito";
-			break;
-		case "enamorado":
-			estado = "enamorado";
-			break;
-		case "atrapado":
-			estado = "atrapado";
-			break;
-		case "drenado":
-			estado = "drenado";
-			break;
-		case "canto mortal":
-			estado = "canto mortal";
-			break;
-		case "paralizado":
-			estado = "paralizado";
-			break;
-		case "envenenado":
-			estado = "envenenado";
-			break;
-		case "gravemente envenenado":
-			estado = "gravemente envenenado";
-			break;
-		case "congelado":
-			estado = "congelado";
-			break;
-		case "dormido":
-			estado = "dormido";
-			break;
-		case "quemado":
-			estado = "quemado";
-			break;
-		}
+	public String getEstado() {
 		return estado;
 	}
 	
